37359aad042d0ab5c9e92bf9785f808f07f7961a
admin <lplove@admins-Mac.local>
2015-03-22 16:03:31 +0800

    Initial Commit

---
+++ MaJiangProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MaJiangProject
+//
+//  Created by admin on 15-3-22.
+//  Copyright (c) 2015年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MaJiangProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MaJiangProject
+//
+//  Created by admin on 15-3-22.
+//  Copyright (c) 2015年 admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
